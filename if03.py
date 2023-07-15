a23b53c60bf839f7babc05a04503679211a42ae7
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-15 13:39:13 +0500

    oraliqdagi son

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if (a>=b and a<=c) or (a<=b and a>=c):
+        return a
+    if (b>=a and b<=c) or (b<=a and b>=c):
+        return b
+    else:
+        return c
+print(main(3,7,1))
+print(main(5,5,-1))
