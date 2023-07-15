ae1b2dcfa778356c27393205966f2e657e98e5ad
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-15 13:56:44 +0500

    index

--- if06.py
+++ if06.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=n//1000%10
+    c=n//100%10
+    d=n//10%10
+    e=n%10
+    if (a>b and a>c and a>d and a>e):
+        return 1
+    if (b>a and b>c and b>d and b>e):
+        return 2
+    if (c>a and c>b and c>d and c>e):
+        return 3
+    if (d>a and d>b and d>c and d>e):
+        return 4
+    if (e>a and e>b and e>c and e>d):
+        return 5
+print(main(23541))
