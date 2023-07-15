43ce906b37a242adb6fe9b01e5fae013c04aca33
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-15 13:53:17 +0500

    besh xonali son kattasi

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=n//1000%10
+    c=n//100%10
+    d=n//10%10
+    e=n%10
+    if (a>b and a>c and a>d and a>e):
+        return a
+    if (b>a and b>c and b>d and b>e):
+        return b
+    if (c>a and c>b and c>d and c>e):
+        return c
+    if (d>a and d>b and d>c and d>e):
+        return d
+    if (e>a and e>b and e>c and e>d):
+        return e
+print(main(23546))
